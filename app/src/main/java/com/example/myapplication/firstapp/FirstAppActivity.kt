10ee7010d376b6e31e77605e2da0fd226ff5fbe6
DANCHUSKY <danielmorenodoncel8@gmail.com>
2023-12-12 16:05:08 +0100

    logcat

--- app/src/main/java/com/example/myapplication/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/example/myapplication/firstapp/FirstAppActivity.kt
@@ -2,11 +2,18 @@ package com.example.myapplication.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
 import com.example.myapplication.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        btnStart.setOnClickListener{
+            Log.i("daniel", "boton pulsado")
+        }
     }
 }
